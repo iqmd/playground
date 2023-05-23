590c0091c9697e7548130b2eda1db0c60417976b
iqbal <iqbal>
2023-05-23 12:55:30 +0530

    edited BST

--- BST/main.c
+++ BST/main.c
@@ -6,43 +6,35 @@ struct tree{
     struct tree *left;
     struct tree *right;
 };
-typedef struct queue{
-    int data;
-    struct queue *next;
-} queue;
-queue* createqueue(){
-   return malloc(sizeof(queue));
-}
-//void buildTree(int A[], int n,struct tree* root){
-//    root = (struct tree*)malloc(sizeof(struct tree));
-//}
-queue *addinq(int data,queue *q){
-    queue *node = malloc(sizeof(queue));
-    node->data = data;
-    node->next = NULL;
-    q = node;
-    return q;
-}
-void displayq(queue *q){
-    while(q){
-        printf(" %d ->",q->data);
-        q = q->next;
-    }
+struct tree* root;
+void insert(struct tree* root, int data){
 
+    if(root == NULL){
+        struct tree *node = malloc(sizeof(struct tree));
+        node->data = data;
+        node->left = node->right = NULL;
+        root = node;
+    } else if (root->data < data) {
+        insert(root->right, data);
+    } else {
+        insert(root->left, data);
+    }
+}
+
+void print(struct tree* root){
+    if(root == NULL){
+        return;
+    }
+    printf("%d",root->data);
+    print(root->left);
+    print(root->right);
 }
 int main(){
+    insert(&root,23);
+    insert(&root,12);
+    insert(&root,2);
+    insert(&root,89);
+    print(root);
 
-    queue *q = createqueue();
-    queue *head = NULL, *tail = NULL;
-    int A[] = {2,3,4,5,6,7};
-    for(int i = 0; i < 6; i++){
-        if(head == NULL){
-            head = addinq(A[i],q);
-            tail = head;
-        }else{
-
-        }
-    }
-    displayq(q);
     return 0;
 }
